2edcfe23e318080ec278ca001d6aa3c7a18fcf10
imyiren <imyiren4@gmail.com>
2019-12-29 15:47:29 +0800

    quick sort

--- src/main/java/io/ilss/datastructure/sort/Main.java
+++ src/main/java/io/ilss/datastructure/sort/Main.java
@@ -11,23 +11,21 @@ public class Main {
 
     public static void main(String[] args) {
         for (int j = 0; j < 10; j++) {
-            System.out.println("-------- " + j + "--------");
+            System.out.println("-------- " + j + " --------");
             // 初始化数据
             long start = 0L;
             long end = 0L;
-            int[] bubbleArr = new int[80000];
-            int[] selectArr = new int[80000];
-            int[] insertArr = new int[80000];
-            int[] shellArr = new int[80000];
-            for (int i = 0; i < 80000; i++) {
-                bubbleArr[i] = (int) (Math.random() * 80000);
-                selectArr[i] = (int) (Math.random() * 80000);
-                insertArr[i] = (int) (Math.random() * 80000);
-                shellArr[i] = (int) (Math.random() * 80000);
-            }
-            int[] shellArr2 = Arrays.copyOf(shellArr, selectArr.length);
-            int[] bubbleArr2 = Arrays.copyOf(bubbleArr, bubbleArr.length);
+            int[] bubbleArr = new int[100000];
 
+            for (int i = 0; i < 100000; i++) {
+                bubbleArr[i] = (int) (Math.random() * 100000);
+            }
+            int[] shellArr2 = Arrays.copyOf(bubbleArr, bubbleArr.length);
+            int[] bubbleArr2 = Arrays.copyOf(bubbleArr, bubbleArr.length);
+            int[] selectArr = Arrays.copyOf(bubbleArr, bubbleArr.length);
+            int[] insertArr = Arrays.copyOf(bubbleArr, bubbleArr.length);
+            int[] shellArr = Arrays.copyOf(bubbleArr, bubbleArr.length);
+            int[] quickArr = Arrays.copyOf(bubbleArr, bubbleArr.length);
 
             System.out.println("===> 默认");
             start = System.currentTimeMillis();
@@ -49,6 +47,11 @@ public class Main {
             ShellSort.sort(shellArr);
             end = System.currentTimeMillis();
             System.out.println("希尔: " + (end - start) / 1000.0);
+
+            start = System.currentTimeMillis();
+            QuickSort.sort(quickArr);
+            end = System.currentTimeMillis();
+            System.out.println("快排: " + (end - start) / 1000.0);
 
             System.out.println("===> 优化后");
             start = System.currentTimeMillis();
---
+++ src/main/java/io/ilss/datastructure/sort/QuickSort.java
@@ -0,0 +1,94 @@
+package io.ilss.datastructure.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author feng
+ */
+public class QuickSort {
+    public static void sort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int left, int right) {
+        int leftPtr = left, rightPtr = right;
+        // 中轴
+        int pivot = arr[(left + right) / 2];
+        int temp;
+        // 只要左右两边没有循环
+        while (leftPtr < rightPtr) {
+            while (arr[leftPtr] < pivot) {
+                leftPtr++;
+            }
+            while (arr[rightPtr] > pivot) {
+                rightPtr--;
+            }
+            if (leftPtr >= rightPtr) {
+                break;
+            }
+
+            temp = arr[leftPtr];
+            arr[leftPtr] = arr[rightPtr];
+            arr[rightPtr] = temp;
+
+            if (arr[leftPtr] == pivot) {
+                rightPtr--;
+            }
+            if (arr[rightPtr] == pivot) {
+                leftPtr++;
+            }
+        }
+
+        if (leftPtr == rightPtr) {
+            leftPtr++;
+            rightPtr--;
+        }
+
+        if (left < rightPtr) {
+            sort(arr, left, rightPtr);
+        }
+        if (right > leftPtr) {
+            sort(arr, leftPtr, right);
+        }
+
+    }
+
+    public static void sort2(int[] arr) {
+        sort2(arr, 0, arr.length - 1);
+    }
+
+    private static void sort2(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        int i = left;
+        int j = right;
+        while (i < j) {
+            while ((i < j) && (arr[j] > pivot)) {
+                j--;
+            }
+            while ((i < j) && (arr[i] < pivot)) {
+                i++;
+            }
+            if ((arr[i] == arr[j]) && (i < j)) {
+                i++;
+            } else {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        if (i - 1 > left) {
+            sort2(arr, left, i - 1);
+        }
+        if (j + 1 < right) {
+            sort2(arr, j + 1, right);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{11, 3, 4, 1, 2, 3, 5, 33, 22};
+        sort2(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
