8281f6804a55c0d8a64d46ab32bc7ea424c9bf5e
imyiren <imyiren4@gmail.com>
2019-12-29 17:29:41 +0800

    merge sort

--- src/main/java/io/ilss/datastructure/sort/Main.java
+++ src/main/java/io/ilss/datastructure/sort/Main.java
@@ -13,12 +13,13 @@ public class Main {
         for (int j = 0; j < 10; j++) {
             System.out.println("-------- " + j + " --------");
             // 初始化数据
-            long start = 0L;
-            long end = 0L;
-            int[] bubbleArr = new int[100000];
+            long start;
+            long end;
+            final int len = 10000000;
+            int[] bubbleArr = new int[len];
 
-            for (int i = 0; i < 100000; i++) {
-                bubbleArr[i] = (int) (Math.random() * 100000);
+            for (int i = 0; i < len; i++) {
+                bubbleArr[i] = (int) (Math.random() * len);
             }
             int[] shellArr2 = Arrays.copyOf(bubbleArr, bubbleArr.length);
             int[] bubbleArr2 = Arrays.copyOf(bubbleArr, bubbleArr.length);
@@ -26,22 +27,26 @@ public class Main {
             int[] insertArr = Arrays.copyOf(bubbleArr, bubbleArr.length);
             int[] shellArr = Arrays.copyOf(bubbleArr, bubbleArr.length);
             int[] quickArr = Arrays.copyOf(bubbleArr, bubbleArr.length);
+            int[] mergeArr = Arrays.copyOf(bubbleArr, bubbleArr.length);
 
             System.out.println("===> 默认");
-            start = System.currentTimeMillis();
-            BubbleSort.sort(bubbleArr);
-            end = System.currentTimeMillis();
-            System.out.println("冒泡: " + (end - start) / 1000.0);
+            if (len < 100000) {
+                start = System.currentTimeMillis();
+                BubbleSort.sort(bubbleArr);
+                end = System.currentTimeMillis();
+                System.out.println("冒泡: " + (end - start) / 1000.0);
 
-            start = System.currentTimeMillis();
-            SelectSort.sort(selectArr);
-            end = System.currentTimeMillis();
-            System.out.println("选择: " + (end - start) / 1000.0);
+                start = System.currentTimeMillis();
+                SelectSort.sort(selectArr);
+                end = System.currentTimeMillis();
+                System.out.println("选择: " + (end - start) / 1000.0);
 
-            start = System.currentTimeMillis();
-            InsertSort.sort(insertArr);
-            end = System.currentTimeMillis();
-            System.out.println("插入: " + (end - start) / 1000.0);
+                start = System.currentTimeMillis();
+                InsertSort.sort(insertArr);
+                end = System.currentTimeMillis();
+                System.out.println("插入: " + (end - start) / 1000.0);
+
+            }
 
             start = System.currentTimeMillis();
             ShellSort.sort(shellArr);
@@ -53,11 +58,19 @@ public class Main {
             end = System.currentTimeMillis();
             System.out.println("快排: " + (end - start) / 1000.0);
 
-            System.out.println("===> 优化后");
             start = System.currentTimeMillis();
-            BubbleSort.sort2(bubbleArr2);
+            MergeSort.sort(mergeArr);
             end = System.currentTimeMillis();
-            System.out.println("冒泡: " + (end - start) / 1000.0);
+            System.out.println("归并: " + (end - start) / 1000.0);
+
+            System.out.println("===> 优化后");
+
+            if (len < 100000) {
+                start = System.currentTimeMillis();
+                BubbleSort.sort2(bubbleArr2);
+                end = System.currentTimeMillis();
+                System.out.println("冒泡: " + (end - start) / 1000.0);
+            }
 
             start = System.currentTimeMillis();
             ShellSort.sort2(shellArr2);
