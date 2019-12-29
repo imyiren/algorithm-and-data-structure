bf34b6a2bfed9cd4b3ba98a327aa4d1cb3a93dd1
imyiren <imyiren4@gmail.com>
2019-12-29 14:35:15 +0800

    shell sort

--- src/main/java/io/ilss/datastructure/sort/InsertSort.java
+++ src/main/java/io/ilss/datastructure/sort/InsertSort.java
@@ -7,15 +7,23 @@ import java.util.Arrays;
  */
 public class InsertSort {
     public static void sort(int[] arr) {
+        // 每一轮要插入的元素
         int insertValue;
+        // 需要插入的位置
         int idx;
         for (int i = 1; i < arr.length; i++) {
+            // 记录需要插入的元素
             insertValue = arr[i];
+            // 记录需要插入的位置
             idx = i;
+            // j >= 0 && insertValue < arr[j] 只要位置大于零 且 插入的值小于当前的值就继续移位
             for (int j = i - 1; j >= 0 && insertValue < arr[j]; j--) {
+                // 往后移
                 arr[j + 1] = arr[j];
+                // 记录当前移动的项的位置
                 idx = j;
             }
+            // 最后把要插入的值放入要插入的位置
             arr[idx] = insertValue;
         }
     }
--- src/main/java/io/ilss/datastructure/sort/Main.java
+++ src/main/java/io/ilss/datastructure/sort/Main.java
@@ -2,6 +2,8 @@ package io.ilss.datastructure.sort;
 
 import com.sun.org.apache.bcel.internal.generic.Select;
 
+import java.util.Arrays;
+
 /**
  * @author feng
  */
@@ -9,20 +11,23 @@ public class Main {
 
     public static void main(String[] args) {
         for (int j = 0; j < 10; j++) {
-            System.out.println("----> " + j);
+            System.out.println("-------- " + j + "--------");
             // 初始化数据
             long start = 0L;
             long end = 0L;
             int[] bubbleArr = new int[80000];
-            int[] bubbleArr2 = new int[80000];
             int[] selectArr = new int[80000];
             int[] insertArr = new int[80000];
+            int[] shellArr = new int[80000];
             for (int i = 0; i < 80000; i++) {
                 bubbleArr[i] = (int) (Math.random() * 80000);
-                bubbleArr2[i] = (int) (Math.random() * 80000);
                 selectArr[i] = (int) (Math.random() * 80000);
                 insertArr[i] = (int) (Math.random() * 80000);
+                shellArr[i] = (int) (Math.random() * 80000);
             }
+            int[] shellArr2 = Arrays.copyOf(shellArr, selectArr.length);
+            int[] bubbleArr2 = Arrays.copyOf(bubbleArr, bubbleArr.length);
+
 
             System.out.println("===> 默认");
             start = System.currentTimeMillis();
@@ -40,11 +45,22 @@ public class Main {
             end = System.currentTimeMillis();
             System.out.println("插入: " + (end - start) / 1000.0);
 
+            start = System.currentTimeMillis();
+            ShellSort.sort(shellArr);
+            end = System.currentTimeMillis();
+            System.out.println("希尔: " + (end - start) / 1000.0);
+
             System.out.println("===> 优化后");
             start = System.currentTimeMillis();
-            BubbleSort.sort2(bubbleArr);
+            BubbleSort.sort2(bubbleArr2);
             end = System.currentTimeMillis();
             System.out.println("冒泡: " + (end - start) / 1000.0);
+
+            start = System.currentTimeMillis();
+            ShellSort.sort2(shellArr2);
+            end = System.currentTimeMillis();
+            System.out.println("希尔: " + (end - start) / 1000.0);
+
         }
     }
 }
