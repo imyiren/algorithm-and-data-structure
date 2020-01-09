49f5438ea5480b9a3d0bf6861c4281a4ef82a004
imyiren <imyiren4@gmail.com>
2020-01-09 22:32:09 +0800

    leetcode 27

---
+++ src/main/java/io/ilss/datastructure/leetcode/array/Q27.java
@@ -0,0 +1,14 @@
+package io.ilss.datastructure.leetcode.array;
+
+/**
+ * @author feng
+ */
+public class Q27 {
+    public int removeElement(int[] nums, int val) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return 1;
+    }
+}
