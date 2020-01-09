3fd807b3fd1c71cebb837b7034e2d7136c1a5dc3
imyiren <imyiren4@gmail.com>
2020-01-09 21:23:57 +0800

    leetcode 16 18

---
+++ src/main/java/io/ilss/datastructure/leetcode/Main.java
@@ -0,0 +1,10 @@
+package io.ilss.datastructure.leetcode;
+
+/**
+ * @author feng
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
