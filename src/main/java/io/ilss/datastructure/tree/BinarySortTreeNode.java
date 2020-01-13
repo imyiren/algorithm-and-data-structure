6c5ad030353ef08ca0cdaeac5d7365d8df6eaa5d
imyiren <imyiren4@gmail.com>
2020-01-13 21:08:27 +0800

    binary sort tree

---
+++ src/main/java/io/ilss/datastructure/tree/BinarySortTreeNode.java
@@ -0,0 +1,22 @@
+package io.ilss.datastructure.tree;
+
+/**
+ * @author feng
+ */
+public class BinarySortTreeNode {
+    public int value;
+    public BinarySortTreeNode left;
+    public BinarySortTreeNode right;
+
+
+    public BinarySortTreeNode(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "BinarySortTreeNode{" +
+                "value=" + value +
+                '}';
+    }
+}
