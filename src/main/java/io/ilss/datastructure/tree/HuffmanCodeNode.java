f2db6c55aac404a539950c317db47d6f0f2bb8cf
imyiren <imyiren4@gmail.com>
2020-01-13 15:54:12 +0800

    zip and decode huffman codes

---
+++ src/main/java/io/ilss/datastructure/tree/HuffmanCodeNode.java
@@ -0,0 +1,29 @@
+package io.ilss.datastructure.tree;
+
+/**
+ * @author feng
+ */
+public class HuffmanCodeNode implements Comparable<HuffmanCodeNode>{
+    public Byte data;
+    public Integer weight;
+    public HuffmanCodeNode left;
+    public HuffmanCodeNode right;
+
+    public HuffmanCodeNode(Byte data, Integer weight) {
+        this.data = data;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(HuffmanCodeNode o) {
+        return this.weight - o.weight;
+    }
+
+    @Override
+    public String toString() {
+        return "HuffmanCodeNode{" +
+                "data=" + data +
+                ", weight=" + weight +
+                '}';
+    }
+}
